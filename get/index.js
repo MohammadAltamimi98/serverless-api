e02373fa74ad4f66b4d4e2cc931f282ad7b6e88b
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-07-27 14:12:51 +0300

    commit

--- get/index.js
+++ get/index.js
@@ -6,9 +6,24 @@ const peopleModel = require('./people.schema');
 
 exports.handler = async (event) => {
   try {
-    // event.body
+    // event.body={ name:'mohammd',age: 23, occupation: 'student'}
+
+    //1. destructre the event.body and parse it
+    const { name, age, occupation } = JSON.parse(event.body);
+    const id = uuid();
+    const doc = new peopleModel({ id, name, age, occupation });
+
+    const data = await doc.save();
+    return {
+      statusCode: 201, // for creation
+      body: JSON.stringify(data)
+    }
+
 
   } catch (error) {
-
+    return {
+      status: 500,
+      message: error.message
+    }
   }
 }

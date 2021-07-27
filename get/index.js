70adff7fbecb1d5f99fe6447b0bb438ad30b4fff
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-07-27 14:04:28 +0300

    commit

---
+++ get/index.js
@@ -0,0 +1,14 @@
+// this will be our lambda handle file
+
+const uuid = require('uuid').v4;
+const peopleModel = require('./people.schema');
+
+
+exports.handler = async (event) => {
+  try {
+    // event.body
+
+  } catch (error) {
+
+  }
+}
